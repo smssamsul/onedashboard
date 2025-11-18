c617bcca8172b7f89c34412e49bf1be031b19e2e
smssamsul <smssamsul@gmail.com>
2025-11-18 16:02:56 +0700

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Admin Dashboard') - One Dashboard</title>
-    <link rel="stylesheet" href="{{ asset('css/admin.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/admin.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.0/dist/chart.umd.min.js"></script>
     @stack('styles')
 </head>
@@ -61,7 +61,7 @@
         </div>
     </div>
 
-    <script src="{{ asset('js/admin.js') }}"></script>
+    <script src="{{ secure_asset('js/admin.js') }}"></script>
     @stack('scripts')
 </body>
 </html>
