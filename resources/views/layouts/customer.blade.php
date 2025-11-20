83816627e3ab0c6a5f19478321b03f7b0f11645d
smssamsul <smssamsul@gmail.com>
2025-11-20 08:35:33 +0700

    asset -> secure_asset

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dashboard Customer') - One Dashboard</title>
-    <link rel="stylesheet" href="{{ asset_secure('css/admin.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('css/admin.css') }}">
     @stack('styles')
 </head>
 <body>
